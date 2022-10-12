a5965c2d78bfaa4d8b9eb864ea395b52de895ca9
DKKs <eye.q.21@gmail.com>
2022-10-12 20:53:57 +0000

    [Add] fix bug onload page, meta og

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# black-page
+# black-screen-saver
 
 [https://bscreen.netlify.app](https://bscreen.netlify.app/)
 
+Online Full Black Screen tool for create full black screen same as a like screensaver.
+
+made with ❤️ 
+
+[mjkodonk](https://github.com/mjkodkks)
--- js/main.js
+++ js/main.js
@@ -1,123 +1,121 @@
-(() => {
-    const brightnessRange = document.getElementById('brightness');
-    const bg = document.getElementById('bg');
-    const container = document.getElementById('container');
-    const svg = document.getElementById('svg');
-    const body = document.documentElement;
-    const fullScreen = document.getElementById('fullScreen');
-    const wordEnter = document.getElementById('wordEnter');
-    const wordExit = document.getElementById('wordExit');
-    const expand = document.getElementById('expand');
-    const close = document.getElementById('close');
+const brightnessRange = document.getElementById('brightness');
+const bg = document.getElementById('bg');
+const container = document.getElementById('container');
+const svg = document.getElementById('svg');
+const body = document.documentElement;
+const fullScreen = document.getElementById('fullScreen');
+const wordEnter = document.getElementById('wordEnter');
+const wordExit = document.getElementById('wordExit');
+const expand = document.getElementById('expand');
+const close = document.getElementById('close');
 
-    window.onload = function() {
-        inactivityTime(); 
+window.onload = function () {
+    resetTimer()
+    inactivityTime();
+    wordEnter.style.display = 'flex';
+    wordExit.style.display = 'none';
+    expand.style.display = 'flex';
+    close.style.display = 'none';
+
+}
+
+function setFullBright() {
+    bg.style.opacity = 0;
+    svg.style.filter = `invert(0)`;
+    brightness.value = 100;
+    fullScreen.style.filter = `invert(0)`;
+}
+
+function requestFullScreen(element) {
+    console.log('in requestFullScreen')
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) {
+        requestMethod.call(element);
+        wordEnter.style.display = 'none';
+        wordExit.style.display = 'flex';
+        expand.style.display = 'none';
+        close.style.display = 'flex';
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+
+function closeFullscreen(element) {
+    console.log('in closeFullscreen')
+    var requestMethod = element.exitFullscreen || element.webkitExitFullscreen || element.mozExitFullscreen || element.msExitFullscreen;
+    console.log(requestMethod);
+    if (requestMethod) {
+        requestMethod.call(element);
         wordEnter.style.display = 'flex';
         wordExit.style.display = 'none';
         expand.style.display = 'flex';
         close.style.display = 'none';
-
-      }
-
-      function setFullBright() {
-        bg.style.opacity = 0;
-        svg.style.filter = `invert(0)`;
-        brightness.value = 100;
-        fullScreen.style.filter = `invert(0)`;
-    }
-
-    function requestFullScreen(element) {
-        console.log('in requestFullScreen')
-        var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
-    
-        if (requestMethod) { 
-            requestMethod.call(element);
-            wordEnter.style.display = 'none';
-            wordExit.style.display = 'flex';
-            expand.style.display = 'none';
-            close.style.display = 'flex';
-        } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-            var wscript = new ActiveXObject("WScript.Shell");
-            if (wscript !== null) {
-                wscript.SendKeys("{F11}");
-            }
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
         }
     }
+}
 
-    function closeFullscreen(element) {
-        console.log('in closeFullscreen')
-        var requestMethod = element.exitFullscreen || element.webkitExitFullscreen || element.mozExitFullscreen || element.msExitFullscreen;
-        console.log(requestMethod);
-        if (requestMethod) { 
-            requestMethod.call(element);
-            wordEnter.style.display = 'flex';
-            wordExit.style.display = 'none';
-            expand.style.display = 'flex';
-            close.style.display = 'none';
-        } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-            var wscript = new ActiveXObject("WScript.Shell");
-            if (wscript !== null) {
-                wscript.SendKeys("{F11}");
-            }
-        }
-    }
+brightness.oninput = (e) => {
+    const rangeInput = e.target.value;
+    bg.style.opacity = 1 - (+rangeInput / 100);
+    svg.style.filter = `invert(${1 - (+rangeInput / 100)}`;
+    fullScreen.style.filter = `invert(${1 - (+rangeInput / 100)}`;
+}
 
-    brightness.oninput = (e)=> {
-        const rangeInput = e.target.value;
-        bg.style.opacity = 1 - (+rangeInput / 100);
-        svg.style.filter = `invert(${1 - (+rangeInput / 100)}`;
-        fullScreen.style.filter = `invert(${1 - (+rangeInput / 100)}`;
-    }
 
-    const inactivityTime = ()=> {
-        var time;
-        window.onload = resetTimer;
-        // DOM Events
-        document.onmousemove = resetTimer;
-        document.onkeypress = resetTimer;
-    
-        function toolLeave() {
-            brightnessRange.classList.remove("fade-in");
-            brightnessRange.classList.add("fade-out");
-            fullScreen.classList.remove("fade-in");
-            fullScreen.classList.add("fade-out");
+let time;
+const inactivityTime = () => {
+    // DOM Events
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+};
 
-            setTimeout(()=> {
-                brightnessRange.style.display = 'flex'
-                fullScreen.style.display = 'flex'
-            },300)
-        }
-    
-        function resetTimer() {
-            clearTimeout(time);
-            brightnessRange.classList.remove("fade-out");
-            fullScreen.classList.remove("fade-out");
-            brightnessRange.classList.add("fade-in");
-            fullScreen.classList.add("fade-in");
-            brightnessRange.style.display = 'flex'
-            fullScreen.style.display = 'flex'
+function toolLeave() {
+    brightnessRange.classList.remove("fade-in");
+    brightnessRange.classList.add("fade-out");
+    fullScreen.classList.remove("fade-in");
+    fullScreen.classList.add("fade-out");
 
-            time = setTimeout(toolLeave, 3000)
-        }
-    };
+    setTimeout(() => {
+        brightnessRange.style.display = 'flex'
+        fullScreen.style.display = 'flex'
+    }, 300)
+}
 
-    svg.onclick = ()=> {
-        setFullBright();
-    }
+function resetTimer() {
+    clearTimeout(time);
+    brightnessRange.classList.remove("fade-out");
+    fullScreen.classList.remove("fade-out");
+    brightnessRange.classList.add("fade-in");
+    fullScreen.classList.add("fade-in");
+    brightnessRange.style.display = 'flex'
+    fullScreen.style.display = 'flex'
 
-    wordEnter.onclick = ()=> {
-        requestFullScreen(body);
-    }
+    time = setTimeout(toolLeave, 3000)
+}
 
-    wordExit.onclick = ()=> {
-        closeFullscreen(document);
-    }
-    expand.onclick = ()=> {
-        requestFullScreen(body);
-    }
+svg.onclick = () => {
+    setFullBright();
+}
 
-    close.onclick = ()=> {
-        closeFullscreen(document);
-    }
+wordEnter.onclick = () => {
+    requestFullScreen(body);
+}
 
-})();
+wordExit.onclick = () => {
+    closeFullscreen(document);
+}
+expand.onclick = () => {
+    requestFullScreen(body);
+}
+
+close.onclick = () => {
+    closeFullscreen(document);
+}
